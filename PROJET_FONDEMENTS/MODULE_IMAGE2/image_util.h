037d7cb691025cc2615b36048f19284e4f14b854
Kmy0609 <camille.baillon.0609@gmail.com>
2017-02-16 15:40:31 +0100

    Update image_util.h

--- PROJET_FONDEMENTS/MODULE_IMAGE2/image_util.h
+++ PROJET_FONDEMENTS/MODULE_IMAGE2/image_util.h
@@ -12,8 +12,33 @@
 #include "type_obj.h"
 #include "exit_if.h"
 
+
+/**
+ * It draws a rectangle from the top left point (xmin,ymin) to the bottom right
+ * point (xmax,ymax) in a chosen color.
+ *
+ * @param picture the image on which the square will be drawn.
+ * @param xmin abscissa of the top left point.
+ * @param ymin ordinate of the top left point.
+ * @param xmax abscissa of the bottom right point.
+ * @param ymax ordinate of the bottom right point.
+ * @param color the color of the square.
+ */
+
 extern void draw_square(image picture, int xmin, int ymin, int xmax, int ymax, unsigned char* color);
 
+/**
+ * It calculates the momenta in a chosen rectangle.
+ *
+ * @param picture the image on which the square is drawn.
+ * @param xmin abscissa of the top left point.
+ * @param ymin ordinate of the top left point.
+ * @param xmax abscissa of the bottom right point.
+ * @param ymax ordinate of the bottom right point.
+ * @param m0 the null order momentum (number of pixel in the square).
+ * @param m1 the 1st order momentum (sum of the intensities of all the pixels in each color).
+ * @param m0 the 2nd order momentum (sum of the square of the intensities of all the pixels in each color).
+ */
 
 extern void give_moments(image picture,int xmin,int ymin,int xmax,int ymax,double* m0,double* m1,double* m2);
 
